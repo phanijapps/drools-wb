440cd29d99a29199f8a4db3f8d63b0a0a40eb291
PhaniJ <phanijapps@gmail.com>
2022-11-13 15:24:31 -0500

    Test

---
+++ kieserver/src/main/java/org/acme/kie/kieserver/KieserverApplication.java
@@ -0,0 +1,13 @@
+package org.acme.kie.kieserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieserverApplication.class, args);
+	}
+
+}
---
+++ kieserver/src/test/java/org/acme/kie/kieserver/KieserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acme.kie.kieserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KieserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
